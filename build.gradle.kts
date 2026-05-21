8f31e4a4150a21ecdc68711c771d7384b4361e57
Aman Kumar Maurya <amankrmj@gmail.com>
2026-05-21 18:37:58 +0530

    update build.gradle.kts to include additional dependencies for security, validation, JWT, MapStruct, Swagger, and monitoring

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,20 +20,52 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-actuator")
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.boot:spring-boot-starter-security")
+    
+    // Spring Boot
     implementation("org.springframework.boot:spring-boot-starter-web")
-    compileOnly("org.projectlombok:lombok")
-    developmentOnly("org.springframework.boot:spring-boot-devtools")
-    runtimeOnly("io.micrometer:micrometer-registry-prometheus")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    
+    // PostgreSQL
     runtimeOnly("org.postgresql:postgresql")
+    
+    // Lombok
+    compileOnly("org.projectlombok:lombok")
     annotationProcessor("org.projectlombok:lombok")
+    
+    testCompileOnly("org.projectlombok:lombok")
+    testAnnotationProcessor("org.projectlombok:lombok")
+    
+    // JWT
+    implementation("io.jsonwebtoken:jjwt-api:0.12.6")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.6")
+    
+    // MapStruct
+    implementation("org.mapstruct:mapstruct:1.6.3")
+    annotationProcessor("org.mapstruct:mapstruct-processor:1.6.3")
+    
+    // Lombok + MapStruct Binding
+    annotationProcessor("org.projectlombok:lombok-mapstruct-binding:0.2.0")
+    
+    // Swagger / OpenAPI
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.9")
+    
+    // Jackson Java Time
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
+    
+    // Monitoring
+    runtimeOnly("io.micrometer:micrometer-registry-prometheus")
+    
+    // Development
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    
+    // Testing
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.security:spring-security-test")
-    testCompileOnly("org.projectlombok:lombok")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-    testAnnotationProcessor("org.projectlombok:lombok")
 }
 
 tasks.withType<Test> {
